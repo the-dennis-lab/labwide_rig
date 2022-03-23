22d0e3e9f0fc405a668f806aead53cd5f47f6991
jonnew <jpnewman@mit.edu>
2022-03-23 10:43:56 -0700

    Added Zaber device source and motion sink
    - Also a simple example
    - Want to test on hardware before going further

---
+++ BonsaiPackage1/ZaberMove.cs
@@ -0,0 +1,24 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using Zaber.Motion.Ascii;
+
+namespace BonsaiPackage1
+{
+    [Description("Take a Zaber device as input and then move that device's lockstep")]
+    [WorkflowElementCategory(ElementCategory.Sink)]
+    public class ZaberMove : Sink<Tuple<Device, double>>
+    {
+        public override IObservable<Tuple<Device, double>> Process(IObservable<Tuple<Device, double>> source)
+        {
+            return source.Do(
+                s =>
+                {
+                    var lockstep = s.Item1.GetLockstep(1);
+                    lockstep.MoveRelative(s.Item2);
+                }
+            );
+        }
+    }
+}
