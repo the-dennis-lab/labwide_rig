e6cabbe6786c344f28c558f4161573fb42eb0815
labadmin <labadmin@dennislab-ww1.hhmi.org>
2022-03-24 15:59:36 -0400

    still bad, messing around with ZaberLocation transform node to better understand C#. broken, but useful. need to fix but first need to better understand classes in C#

--- BonsaiPackage1/ZaberLocation.cs
+++ BonsaiPackage1/ZaberLocation.cs
@@ -2,7 +2,6 @@
 using System;
 using System.ComponentModel;
 using System.Reactive.Linq;
-using Zaber.Motion;
 using Zaber.Motion.Ascii;
 using TSource = Zaber.Motion.Ascii.Device;
 using TResult = System.Tuple<double, double>;
@@ -12,21 +11,44 @@ namespace BonsaiPackage1
 {
     [Description("attempting to use a zaber device as input and return the location of the stage in x and y")]
     [WorkflowElementCategory(ElementCategory.Transform)]
+
     public class ZaberLocation : Transform<TSource, TResult>
     {
-        public override IObservable<TResult> Process(TSource source)
+        public override IObservable<TResult> Process(IObservable<TSource> source)
         {
-            // fucking around with this - the variables here are useful
-            // but the implementation is obviously wrong - had to stop mid-destroying it :) 
-            // 
-            var lockstep = source.GetLockstep(1);
-            var lockstepoffset = lockstep.GetOffsets();
-            var axis = source.GetAxis(3);
-            var axisposition = axis.GetPosition();
-            var tuptoreturn = Tuple<T1,T2>(double lockstepoffset, double axisposition);
 
-            return Observable.Return(tuptoreturn);
+            return source.Do(
+                s =>
+                    {
+                        var lockstep = s.GetLockstep(1);
+                        double lockstepoffset = lockstep.GetOffsets()[0];
+                        Console.WriteLine($"lockstepoffset is {lockstepoffset}");
+                        var axis = s.GetAxis(3);
+                        double axisposition = axis.GetPosition();
+                        Console.WriteLine($"axisposition is {axisposition}");
+                        TResult(lockstepoffset, axisposition);
+                    });
+
         }
-
     }
 }
+    
+        /*
+        public void Process(TSource source)
+        {
+            var lockstep = source.GetLockstep(1);
+            double lockstepoffset = lockstep.GetOffsets()[0];
+            Console.WriteLine($"lockstepoffset is {lockstepoffset}");
+            var axis = source.GetAxis(3);
+            double axisposition = axis.GetPosition();
+            Console.WriteLine($"axisposition is {axisposition}");
+            Tuple<double, double> t2 = new Tuple<double, double>(lockstepoffset, axisposition);
+
+        }
+
+        public override IObservable<TResult> Process(IObservable<TSource> source)
+        {
+            return (IObservable<TResult>)source.Do(Process);
+        }
+        */
+   
