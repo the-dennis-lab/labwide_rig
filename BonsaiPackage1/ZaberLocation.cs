403c0d27d6163ac19e32abdbca1c372625bf311d
labadmin <labadmin@dennislab-ww1.hhmi.org>
2022-03-25 09:35:46 -0400

    unfucked the ZaberLocation - used the Transform1 test I wrote previously to take an int and turn it into a sine wave as the structure and combined with the variables I had been making to get the stage position. now outputs a tuple.

--- BonsaiPackage1/ZaberLocation.cs
+++ BonsaiPackage1/ZaberLocation.cs
@@ -14,10 +14,11 @@ namespace BonsaiPackage1
 
     public class ZaberLocation : Transform<TSource, TResult>
     {
+        
         public override IObservable<TResult> Process(IObservable<TSource> source)
         {
 
-            return source.Do(
+            return source.Select(
                 s =>
                     {
                         var lockstep = s.GetLockstep(1);
@@ -26,29 +27,10 @@ namespace BonsaiPackage1
                         var axis = s.GetAxis(3);
                         double axisposition = axis.GetPosition();
                         Console.WriteLine($"axisposition is {axisposition}");
-                        TResult(lockstepoffset, axisposition);
+                        return Tuple.Create(lockstepoffset, axisposition);
                     });
 
-        }
+        } 
     }
 }
     
-        /*
-        public void Process(TSource source)
-        {
-            var lockstep = source.GetLockstep(1);
-            double lockstepoffset = lockstep.GetOffsets()[0];
-            Console.WriteLine($"lockstepoffset is {lockstepoffset}");
-            var axis = source.GetAxis(3);
-            double axisposition = axis.GetPosition();
-            Console.WriteLine($"axisposition is {axisposition}");
-            Tuple<double, double> t2 = new Tuple<double, double>(lockstepoffset, axisposition);
-
-        }
-
-        public override IObservable<TResult> Process(IObservable<TSource> source)
-        {
-            return (IObservable<TResult>)source.Do(Process);
-        }
-        */
-   
