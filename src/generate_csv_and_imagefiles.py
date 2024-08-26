914523b6474e1e22e56a6d8214a70e4f3042da1c
Emily <emilyjanedennis@gmail.com>
2024-08-26 11:42:37 -0400

    updates to auto-generate tiles

---
+++ src/generate_csv_and_imagefiles.py
@@ -0,0 +1,305 @@
+import os, sys, math
+from datetime import datetime
+import tifffile as tif
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import pandas as pd
+import utils
+
+print(sys.argv)
+print(len(sys.argv))
+if len(sys.argv)<3:
+    print('error, you must enter two integers, \n ls and ax3 position for the pole in the top of A1 \n (furthest from the door) ')
+else:
+
+    try:
+        A1_top_ls = int(sys.argv[1])
+        A1_top_ax= int(sys.argv[2])
+    except:
+        print('error, you must enter two integers, ls and ax3 position for the pole in the top of A1 (furthest from the door), you entered {}'.format(sys.argv[1:]))
+
+    print('using {} as A1 top, ls \n using {} as A1 top, ax3 in so-called Native Zaber units'.format(A1_top_ls,A1_top_ax))
+
+    # middle val = top + offset
+    # middle - top = offset
+    middle_from_0_offset = [227900-219000,57300-56600]
+    middle_from_0_offset
+
+    # get A1 center
+    A1c = (A1_top_ls+middle_from_0_offset[0],A1_top_ax+middle_from_0_offset[1])
+    print('a1c is {}'.format(A1c))
+
+    A_ls_cs,A_ax_cs=A1c
+
+    # get B centers (4)
+    B3c = utils.get_neighbor_tile(A1c,1)
+    B_ls_cs,B_ax_cs = utils.get_row_vals(B3c,2,4)
+    C3c=utils.get_neighbor_tile([B_ls_cs[0],B_ax_cs[0]],1)
+    C_ls_cs,C_ax_cs = utils.get_row_vals(C3c,2,7)
+    D3c=utils.get_neighbor_tile([C_ls_cs[0],C_ax_cs[0]],1)
+    D_ls_cs,D_ax_cs = utils.get_row_vals(D3c,2,10)
+    print('D1c is {}'.format([D_ls_cs[0],D_ax_cs[0]]))
+    E3c=utils.get_neighbor_tile([D_ls_cs[0],D_ax_cs[0]],1)
+    E_ls_cs,E_ax_cs = utils.get_row_vals(E3c,2,13)
+    F1c=utils.get_neighbor_tile([E_ls_cs[0],E_ax_cs[0]],1)
+    F_ls_cs,F_ax_cs = utils.get_row_vals(F1c,0,12)
+    print('f1c {}'.format(F1c))
+    G2c=utils.get_neighbor_tile([F_ls_cs[0],F_ax_cs[0]],1)
+    G_ls_cs,G_ax_cs = utils.get_row_vals(G2c,1,13)
+    H1c=utils.get_neighbor_tile([G_ls_cs[0],G_ax_cs[0]],1)
+    H_ls_cs,H_ax_cs = utils.get_row_vals(H1c,0,12)
+    print('H1c {}'.format(H1c))
+    I2c=utils.get_neighbor_tile([H_ls_cs[0],H_ax_cs[0]],1)
+    I_ls_cs,I_ax_cs = utils.get_row_vals(I2c,1,13)
+    J1c=utils.get_neighbor_tile([I_ls_cs[0],I_ax_cs[0]],1)
+    J_ls_cs,J_ax_cs = utils.get_row_vals(J1c,0,12)
+    print('J1c {}'.format(J1c))
+    K2c=utils.get_neighbor_tile([J_ls_cs[0],J_ax_cs[0]],1)
+    K_ls_cs,K_ax_cs = utils.get_row_vals(K2c,1,13)
+    L1c=utils.get_neighbor_tile([K_ls_cs[0],K_ax_cs[0]],1)
+    L_ls_cs,L_ax_cs = utils.get_row_vals(L1c,0,12)
+    print('L1c {}'.format(L1c))
+    M2c=utils.get_neighbor_tile([L_ls_cs[0],L_ax_cs[0]],1)
+    M_ls_cs,M_ax_cs = utils.get_row_vals(M2c,1,13)
+    N1c=utils.get_neighbor_tile([M_ls_cs[0],M_ax_cs[0]],1)
+    N_ls_cs,N_ax_cs = utils.get_row_vals(N1c,0,10)
+    # from N2
+    O1c=utils.get_neighbor_tile([N_ls_cs[1],N_ax_cs[1]],1)
+    O_ls_cs,O_ax_cs = utils.get_row_vals(O1c,0,7)
+    P1c= utils.get_neighbor_tile([O_ls_cs[1],O_ax_cs[1]],1)
+    P_ls_cs,P_ax_cs = utils.get_row_vals(P1c,0,4)
+    #from O2
+    Q1c= utils.get_neighbor_tile([P_ls_cs[1],P_ax_cs[1]],1)
+    Q_ls_cs,Q_ax_cs = Q1c
+    #from P2
+
+######################
+# assemble, plot fig
+all_ax_cs = [[A_ax_cs],B_ax_cs,C_ax_cs,D_ax_cs,E_ax_cs,F_ax_cs,G_ax_cs,H_ax_cs,I_ax_cs,J_ax_cs,K_ax_cs,L_ax_cs,M_ax_cs,N_ax_cs,O_ax_cs,P_ax_cs,[Q_ax_cs]]
+all_ls_cs = [[A_ls_cs],B_ls_cs,C_ls_cs,D_ls_cs,E_ls_cs,F_ls_cs,G_ls_cs,H_ls_cs,I_ls_cs,J_ls_cs,K_ls_cs,L_ls_cs,M_ls_cs,N_ls_cs,O_ls_cs,P_ls_cs,[Q_ls_cs]]
+
+for i in np.arange(0,len(all_ax_cs)):
+    plt.scatter(all_ax_cs[i],all_ls_cs[i])
+plt.scatter(all_ax_cs[0],all_ls_cs[0],s=100)
+
+plt.xlim([0,350000])
+plt.ylim([-5000,345000])
+plt.title('check E2 center is [ls,ax]: {} \n E12 center is {} \n  L10 center is {}'.format((E_ls_cs[1],E_ax_cs[1]),(E_ls_cs[11],E_ax_cs[11]),(L_ls_cs[9],L_ax_cs[9])))
+#plt.scatter(current_file.ax_new_guess,current_file.ls_guess,c='k',alpha=0.2)
+plt.savefig('../../../checkfig.png')
+
+
+############################
+# make csv
+alpha = 'ABCDEFGHIJKLMNOPQ'
+numera = np.arange(1,14).astype(str)
+
+names = []
+ls_guesses = []
+ax_new_guesses = []
+
+for i in np.arange(0,len(all_ax_cs)):
+    curr_ax = all_ax_cs[i]
+    curr_ls = all_ls_cs[i]
+
+    for j in np.arange(0,len(curr_ax)):
+        names.append(alpha[i]+numera[j])
+        ls_guesses.append(int(curr_ls[j]))
+        ax_new_guesses.append(int(curr_ax[j]))
+new_dict = {'name':names,'ls_guess':ls_guesses,'ax_new_guess':ax_new_guesses}
+new_csv = pd.DataFrame(new_dict)
+now = datetime.now()
+filestring = "../../../data/zaber_centers_"+now.strftime("%Y%m%d")+".csv"
+filestring2 = "../../../data/zaber_centers.csv"
+new_csv.to_csv(filestring,index=False)
+try:
+    new_csv.to_csv(filestring2,index=False)
+except:
+    print(' do you have zaber_centers.csv open? if so, close and try again')
+##################################
+# make ls map
+
+# make empty array
+new_ls = np.zeros((3500,3500,3))
+
+# make top white (255), bottom is already black
+top = int(E_ls_cs[0]/100) #divide by 100 bc zaber units are 350,000 not 3500
+new_ls[0:top,0:3500]=255
+
+# make horizontal 128 gray stripe
+top  = int((A1c[0]+58900)/100)
+bottom = int((A1c[0]+237900)/100)
+new_ls[top:bottom,0:3500]=128
+
+# get top left triangle filled as 128
+ls_low = int((A1c[0]-10900)/100)
+ls_high = int((A1c[0]+73700)/100)
+ax_low = int((A1c[1]-129900)/100)
+ax_high = int((A1c[1]-700)/100)
+
+[lss,axs] = utils.getpoints((ls_high,ax_low),(ls_low,ax_high+1)) # returns integer locations on line
+# fill from val to high_ls, ax-1 to ax
+for i in np.arange(1,len(lss)):
+    if axs[i]<3500:
+        new_ls[lss[i]:ls_high,axs[i-1]:axs[i]]=128
+
+# top right triangle fill
+ls_low = int((A1c[0]-10900)/100)
+ls_high = int((A1c[0]+58900)/100)
+ax_low = int((A1c[1]+142300)/100)
+ax_high = int((A1c[1]-700)/100)
+
+[lss,axs] = utils.getpoints((ls_high,ax_low),(ls_low,ax_high+1)) # returns integer locations on line
+
+for i in np.arange(1,len(lss)):
+    if axs[i]<3500:
+        new_ls[lss[i]:ls_high,axs[i-1]:axs[i]]=128
+
+# fill bottom left triangle
+ls_low = int((A1c[0]+236900)/100)
+ls_high = int((A1c[0]+294800)/100)
+ax_low = int((A1c[1]-4100)/100)
+ax_high = int((A1c[1]-121700)/100)
+
+[lss,axs] = utils.getpoints((ls_low,ax_high),(ls_high,ax_low-1)) # returns integer locations on line
+for i in np.arange(1,len(lss)):
+    new_ls[bottom:lss[i],axs[i-1]:axs[i]]=128
+
+# fill bottom right triangle
+ls_low = int((A1c[0]+216900)/100)
+ls_high = int((A1c[0]+294800)/100)
+ax_low = int((A1c[1]+36500)/100)
+ax_high = int((A1c[1]+151700)/100)
+
+[lss,axs] = utils.getpoints((ls_low,ax_high),(ls_high,ax_low-1)) # returns integer locations on line
+for i in np.arange(1,len(lss)):
+    new_ls[bottom:lss[i],axs[i-1]:axs[i]]=128
+
+# fill between triangles
+ax_low = int((A1c[1]-4100)/100)
+
+maxv = int((A1c[1]+436500)/100)
+new_ls[bottom:int((A1c[1]+436500)/100),int((A1c[1]-4100)/100):int((A1c[1]+36500)/100)]=128
+
+ls_tif_filestring='/Users/emily/Desktop/data/ls_limits.png'
+ls_tif_filestring2='/Users/emily/Desktop/data/ls_limits_{}.png'.format(now.strftime("%Y%m%d"))
+tif.imsave(ls_tif_filestring,new_ls.astype('uint8'),photometric='rgb')
+tif.imsave(ls_tif_filestring2,new_ls.astype('uint8'),photometric='rgb')
+
+########################
+# make ax3
+
+# make empty array
+new_ax = np.zeros((3500,3500,3))
+
+# first fill everything right of the middle with white
+# B4 P4 get lowest ax value
+[ls_high,ls_low,ax_high,ax_low]=utils.get_relative_vals(new_csv,"B4","P4",0)
+new_ax[:,ax_low:3500]=255
+
+# next fill a vertical stripe
+[ls_high,ls_low,ax_high,ax_low]=utils.get_relative_vals(new_csv,"B1","B4",0)
+new_ax[0:3500,ax_low:ax_high]=128
+b_left = ax_low
+b_right = ax_high
+
+# next make a box from low_ls between E1/E13 and high ls between M1/M13 and low ax E1/M1 and high ax E13/M13
+a = int((A1c[0]+73600)/100)
+b = int((A1c[0]+237900)/100)
+c = int((A1c[1]-119700)/100)
+d = int((A1c[1]+120400)/100)
+
+if d < 3500:
+    new_ax[a:b,c:d]=128
+else:
+    new_ax[a:b,c:3500]=128
+
+a = int((A1c[0]+58800)/100)
+b = int((A1c[0]+73600)/100)
+c = int(A1c[1]/100)
+d = int((A1c[1]+120400)/100)
+if d < 3500:
+    new_ax[a:b,c:d]=128
+else:
+    new_ax[a:b,c:3500]=128
+
+
+# get top left triangle filled as 128
+ls_low = int((A1c[0]-10900)/100)
+ls_high = int((A1c[0]+73700)/100)
+ax_low = int((A1c[1]-129900)/100)
+ax_high = int((A1c[1]-700)/100)
+
+[lss,axs] = utils.getpoints((ls_high,ax_low),(ls_low,ax_high+1)) # returns integer locations on line
+# fill from val to high_ls, ax-1 to ax
+for i in np.arange(1,len(lss)):
+    if axs[i]<3500:
+        new_ax[lss[i]:ls_high,axs[i-1]:axs[i]]=128
+
+# top right triangle fill
+ls_low = int((A1c[0]-10900)/100)
+ls_high = int((A1c[0]+58900)/100)
+ax_low = int((A1c[1]+142300)/100)
+ax_high = int((A1c[1]-700)/100)
+
+[lss,axs] = utils.getpoints((ls_high,ax_low),(ls_low,ax_high+1)) # returns integer locations on line
+
+for i in np.arange(1,len(lss)):
+    if axs[i]<3500:
+        new_ax[lss[i]:ls_high,axs[i-1]:axs[i]]=128
+
+# fill bottom left triangle
+ls_low = int((A1c[0]+236900)/100)
+ls_high = int((A1c[0]+294800)/100)
+ax_low = int((A1c[1]-4100)/100)
+ax_high = int((A1c[1]-121700)/100)
+
+[lss,axs] = utils.getpoints((ls_low,ax_high),(ls_high,ax_low-1)) # returns integer locations on line
+for i in np.arange(1,len(lss)):
+    new_ax[bottom:lss[i],axs[i-1]:axs[i]]=128
+
+# fill bottom right triangle
+ls_low = int((A1c[0]+216900)/100)
+ls_high = int((A1c[0]+294800)/100)
+ax_low = int((A1c[1]+36500)/100)
+ax_high = int((A1c[1]+151700)/100)
+
+[lss,axs] = utils.getpoints((ls_low,ax_high),(ls_high,ax_low-1)) # returns integer locations on line
+for i in np.arange(1,len(lss)):
+    new_ax[bottom:lss[i],axs[i-1]:axs[i]]=128
+
+# fill between triangles
+ax_low = int((A1c[1]-4100)/100)
+
+maxv = int((A1c[1]+436500)/100)
+new_ax[bottom:int((A1c[1]+436500)/100),int((A1c[1]-4100)/100):int((A1c[1]+36500)/100)]=128
+
+# now add left and right side triangles to finish!
+#left vert triangle
+ls_low = int((A1c[0]+73600)/100)
+ls_high = int((A1c[0]+237900)/100)
+ax_low = int((A1c[1]-119700)/100)
+ax_high = int((A1c[1]-129900)/100)
+[lss,axs] = utils.getpoints((ls_low,ax_high),(ls_high,ax_low-1)) # returns integer locations on line
+
+for i in np.arange(1,len(lss)):
+    new_ax[lss[i-1]:lss[i],axs[i]:ax_low]=128
+
+#right vert triangle
+ls_low = int((A1c[0]+58800)/100)
+ls_high = int((A1c[0]+237900)/100)
+ax_low = int((A1c[1]+120400)/100)
+ax_high = int((A1c[1]+142000)/100)
+[lss,axs] = utils.getpoints((ls_low,ax_low),(ls_high,ax_high))
+print(lss[0],lss[-1],axs[0],axs[-1])
+for i in np.arange(1,len(lss)):
+    new_ax[lss[i-1]:lss[i],ax_low:axs[-i]]=128
+
+### save
+ax_tif_filestring='/Users/emily/Desktop/data/ax_limits.png'
+ax_tif_filestring2 = '/Users/emily/Desktop/data/ax_limits_{}.png'.format(now.strftime("%Y%m%d"))
+tif.imsave(ax_tif_filestring,new_ax.astype('uint8'),photometric='rgb')
+tif.imsave(ax_tif_filestring2,new_ax.astype('uint8'),photometric='rgb')
+
+#
---
+++ src/utils.py
@@ -0,0 +1,149 @@
+import os, sys, math
+from datetime import datetime
+import tifffile as tif
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import pandas as pd
+
+def get_row_vals(centers,index_of_centers,length_of_row):
+    ''' function to auto-generate row values from a single center val
+    INPUTS:
+        1. centers: a tuple or list of center values for a tile in Zaber units (e.g. ls = 238800, ax=47200 the input would be [238800,47200] or (238800,47200))
+        2. index_of_centers: an integer indicating which tile value you're inputting (e.g. M1 = element 0, M2 = element 1... etc)
+        3. length_of_row: an integer indicating how many tiles in the row (e.g. 1 if A, 4 if B... 13 if M... 1 if Q)
+    OUTPUTS:
+        two lists ls_row_vals and ax_row_vals
+    EXAMPLE:
+        M1c = (230100, 46500)
+        M_ls_cs,M_ax_cs = get_row_vals(M1c,0,13)
+        print(M_ls_cs)
+        print(M_ax_cs)
+        > [230100, 228100, 226100, 224100, 222100, 220100, 218100, 216100, 214100, 212100, 210100, 208100, 206100]
+        > [46500, 67700, 88900, 110100, 131300, 152500, 173700, 194900, 216100, 237300, 258500, 279700, 300900]
+
+    NOTE: if you have K2c, try
+        K_ls_cs, K_ax_cs = get_row_vals(K2c,1,13) because K2 is the 1st element in the list of K tile values from 1-13
+    '''
+    ls_row_offset = -1600
+    ax_row_offset= 21600
+    print('using ls_row_offset of {} and ax_row_offset of {}'.format(ls_row_offset,ax_row_offset))
+    ls = centers[0]
+    ax = centers[1]
+    ls_row_vals=[]
+    ax_row_vals=[]
+    for i in np.arange(0-index_of_centers,length_of_row-index_of_centers):
+        ls_row_vals.append(ls+i*ls_row_offset)
+        ax_row_vals.append(ax+i*ax_row_offset)
+    return ls_row_vals, ax_row_vals
+
+def get_neighbor_tile(centers,direction):
+    ''' INPUTS
+        1. centers: a tuple or list of center values for a tile in Zaber units (e.g. ls = 238800, ax=47200 the input would be [238800,47200] or (238800,47200))
+        2. direction: an int, either 0 for above right or 1 for below right (e.g. to get M2 from L2, use 1. to get L1 from M1, use 0)
+
+        TODO later I should make this more flexible and harder to mess up
+        OUTPUTS:
+        1. new list with the center values in [ls,ax]
+    '''
+    ls = centers[0]
+    ax = centers[1]
+    M1_to_L1_offset = [-19500,9200]
+    L1_to_M2_offset = [17600,12200]
+    if direction == 0:
+        new_centers = [ls+M1_to_L1_offset[0],ax+M1_to_L1_offset[1]]
+        #print('generated the tile center above and right of the values provided (e.g. M1 center entered, want L1 centers) using {} offset'.format(M1_to_L1_offset))
+    elif direction == 1:
+        new_centers = [ls+L1_to_M2_offset[0],ax+L1_to_M2_offset[1]]
+        #print('generated the tile center from below and right of the values provided (e.g. L1 center entered, want M2 centers) using {} offset'.format(L1_to_M2_offset))
+    else:
+        print('ERROR: you entered {}, which must be either 0 (for above, right) or 1 (for below, right)')
+        new_centers=[]
+    return new_centers
+
+def get_relative_vals(df, location1, location2, adj_val):
+    '''
+    INPUTS:
+        1. df: a pandas DataFrame with three columns: name, ls_guess, and ax_new_guess
+        2.
+        3.
+        4. adj_val = adjust the outputs by this amount
+    OUTPUTS:
+        1. ls_high the highest ls value as an integer
+        2. ls_low the lowest ls value as an integer
+        3. ax_high the highest ax value as an integer
+        4. ax_low the lowest ax value as an integer
+    '''
+    ax1= int(df.ax_new_guess[df.name==location1]/100)
+    ax2 = int(df.ax_new_guess[df.name==location2]/100)
+    ls1= int(df.ls_guess[df.name==location1]/100)
+    ls2 = int(df.ls_guess[df.name==location2]/100)
+    if ls1 > ls2:
+        ls_high = ls1
+        ls_low = ls2
+    else:
+        ls_low = ls1
+        ls_high = ls2
+    if ax1 > ax2:
+        ax_low = ax2
+        ax_high = ax1
+    else:
+        ax_low = ax1
+        ax_high = ax2
+    return ls_high+adj_val,ls_low-adj_val,ax_high+adj_val,ax_low-adj_val
+
+
+def get_closest(list_input,num):
+    aux = []
+    for val in list_input:
+        aux.append(abs(num-val))
+
+    return aux.index(min(aux))
+
+def getpoints(p1, p2):
+    # Sort both points first.
+    (x1, y1), (x2, y2) = sorted([p1, p2])
+    a = b = 0.0
+    yvals=[]
+    xvals=np.arange(int(x1), int(x2) + 1)
+    # First point is in (0, y).
+    if x1 == 0.0:
+        b = y1
+        a = (y2 - y1) / x2
+    elif x2 == 0.0:
+        # Second point is in (0, y).
+        b = y2
+        a = (y1 - y2) / x1
+    else:
+        # Both points are valid.
+        b = (y2 - (y1 * x2) / x1) / (1 - (x2 / x1))
+        a = (y1 - b) / x1
+        for x in xvals:
+            yvals.append(a * float(x) + b)
+    yvals=[round(val) for val in yvals]
+    axs=[]
+    lss=[]
+    for val in np.arange(np.min(yvals),np.max(yvals)):
+        i=get_closest(yvals,val) # gets integer of yval closest
+        lss.append(xvals[i])
+        axs.append(yvals[i])
+    return (lss, axs)
+
+def rot_list_of_ax_ls(ax_list,ls_list,angle):
+    # first check lists are same length
+    if len(ax_list)!=len(ls_list):
+        print('ERROR, your lists are two different lengths! 1st argument, ax_list is {} and 2nd argument ls_list is {}'.format(len(ax_list),len(ls_list)))
+    else:
+        ax_new=[]
+        ls_new=[]
+
+        ax_com=np.mean(ax_list)
+        ls_com = np.mean(ls_list)
+        ax_corr = ax_list-ax_com
+        ls_corr = ls_list-ls_com
+        print('using angle of {} degrees'.format(angle))
+        alfa = angle*math.pi/180
+        for i in range(len(ax_list)):
+            ax_new.append((math.cos(alfa)*ax_corr[i]-math.sin(alfa)*ls_corr[i])+ax_com)
+            ls_new.append((math.sin(alfa)*ax_corr[i]+math.cos(alfa)*ls_corr[i])+ls_com)
+    return [ax_new,ls_new]
